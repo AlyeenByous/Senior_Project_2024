d3979f36bd40e3616215b5d795a8b9d3ddfd271b
dlg94490 <150751446+dlg94490@users.noreply.github.com>
2024-03-28 15:56:51 -0500

    work on listItems

--- backend/certbackend/listItems.js
+++ backend/certbackend/listItems.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+const requestTable = process.env.REQUEST_TABLE;
+
+exports.listItems = async (event, context, callback) => {
+    let headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+    };
+    let statusCode = 200;
+
+    console.log("EVENT:::", JSON.stringify(event));
+
+    const tableName = event.pathParameters.model
+    let table;
+    switch (tableName) {
+        case "request":
+            table = requestTable;
+            break;
+        default:
+            throw new Error(`Unsupported resource: "${modelName}"`);
+    }
+
+    const params = {
+        TableName: table
+    }
+
+    console.log("Getting Items from table:::", table);
+
+    await dynamoDb.scan(params, (error, data) => {
+        if (error) {
+            console.log('Scan failed. Error JSON:', JSON.stringify(error, null, 2));
+            callback(error);
+            return;
+        }
+        const response = {
+            statusCode,
+            headers,
+            body: JSON.stringify(data.Items)
+        }
+        callback(null, response);
+    }).promise();
+};
