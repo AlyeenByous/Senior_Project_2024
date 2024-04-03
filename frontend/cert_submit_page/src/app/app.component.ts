d2936a07d353eade426452e7480e44520d9fcea6
Ally Byous <alyeenbyous@gmail.com>
2024-04-02 20:51:28 -0500

    Completed the work to get a frontend-only working upload pdf and to get a pdf that you can download, pdf that you download needs some formatting help but I don't have the time left in this sprint to complete it.

--- frontend/cert_submit_page/src/app/app.component.ts
+++ frontend/cert_submit_page/src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+// import * as pdfMake from "pdfmake/build/pdfMake";
+// import * as pdfFonts from "pdfmake/build/vfs_fonts";
+import { BlankPageComponent } from './blank-page/blank-page.component';
+import {parse} from 'node-html-parser';
 
+// (pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'cert_submit_page';
+
+  // generatePDF(){
+  //   let text = (<HTMLInputElement>document.getElementById("employeeName")).value;
+  //   let docDefinition = {
+  //     content:[
+  //       'This does work.'
+  //     ]
+  //   };
+  //   pdfMake.createPdf(docDefinition).open();
+  // }
 }
--- frontend/cert_submit_page/src/app/blank-page/blank-page.component.ts
+++ frontend/cert_submit_page/src/app/blank-page/blank-page.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import * as pdfMake from "pdfmake/build/pdfMake";
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
+import html2canvas from 'html2canvas';
+import { jsPDF } from "jspdf";
+
+
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+
 
 @Component({
   selector: 'app-blank-page',
   templateUrl: './blank-page.component.html',
   styleUrls: ['./blank-page.component.css']
 })
+
+
 export class BlankPageComponent {
+ 
+  //tried two different versions that never worked until I found a different stackechange thread
+
+  
+  // generatePDF(){
+  //   let data = <HTMLElement>document.getElementById("exportform");
+  //   html2canvas(data).then[canvas => {
+  //     const contentDataURL = canvas.toDataURL('image/png');
+  //     let pdf = new jsPDF();
+  //     var position = 10;
+  //     pdf.addImage[contentDataURL, 'PNG',0,position];
+  //     pdf.save('Certificate_Submission.pdf');
+  //   }];
+  // }
+
+  
+  // generatePDF(){
+
+  //   let root = <HTMLElement>document.getElementById('exportform');
+  //   html2canvas(root, {
+  //     onrendered: function (canvas) {
+  //         var data = canvas.toDataURL();
+  //         var docDefinition = {
+  //             content: [{
+  //                 image: data,
+  //                 width: 500,
+  //             }]
+  //         };
+  //         pdfMake.createPdf(docDefinition).download("Score_Details.pdf");
+  //     }
+  // });
+
+  // }
+
+  generatePDF()
+  {
+    //https://stackoverflow.com/questions/55019343/how-to-generate-a-pdf-using-angular-7 basically helped me with this.
+      let data = <HTMLElement>document.getElementById('exportform');  
+      html2canvas(data).then(canvas => {
+      const contentDataURL = canvas.toDataURL('image/png')  // 'image/jpeg' for lower quality output.
+      //let pdf = new jspdf('l', 'cm', 'a4'); //Generates PDF in landscape mode
+      let pdf = new jsPDF('p', 'cm', 'a4'); //Generates PDF in portrait mode
+      pdf.addImage(contentDataURL, 'PNG', 0, 0, 29.7, 21.0);  
+      pdf.save('Cert_Submission.pdf');   
+    }); 
+  }
+
 
 }
+
