0c2c6ee7e196987f100b7a2beefc3816590d9457
dlg94490 <150751446+dlg94490@users.noreply.github.com>
2024-04-06 17:56:48 -0500

    Final Edit before monday
    Um, guys, I'm not sure what to write for this one

--- backend/certbackend/getItem.js
+++ backend/certbackend/getItem.js
@@ -1,25 +1,27 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+//const { URLSearchParams } = require('url');
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 //const uuid = require('uuid'); //requre all fields***
+const requestTable = process.env.REQUEST_TABLE;
 
-module.exports.createRequest = async (event, context, callback) => {
+module.exports.getItem = async (event, context, callback) => {
     let headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Credentials': true
     };
     let statusCode = 200;
 
-    const data = JSON.parse(Buffer.from(event.body, 'base64').toString());
+   // const data = JSON.parse(Buffer.from(event.body, 'base64').toString());
     console.log("EVENT:::", data);
 
-    const tableName = event.pathParameters.model;
+    const tableName =requestTable; // event.pathParameters.model;
     const id = "id";
     let table;
     switch (tableName) { //If you have other tables you would add them here as other case statements to reference that table.
         case "error":
-            throw new Error(`Unsupported resource: "${modelName}"`);
+            throw new Error("Unsupported resource:");
         default:           
             table = requestTable;
             break;
@@ -27,22 +29,23 @@ module.exports.createRequest = async (event, context, callback) => {
 
     const params = {
         TableName: table,
-        KeyConditionExpression: "id = id",
-        ExpressionAttributeValues: "{\":id\":{\"S\":\"i839d\"}}"
-        }
-        
+        KeyConditionExpression: "id = :id",
+        ExpressionAttributeValues:{ ":id" : {S: id.toString() }} //{\":id\":{\"S\":\"i839d\"}
     }
+        
+    
 
     console.log("Getting individual Item from table:::", table);
 
-    await dynamoDb.query(params, function (err, data) {
+    await dynamoDb.query(params, (err, data)=> { 
         if (err) {
-          console.log("Error", err);
-        }
+          console.log("Error",  JSON.stringify(err, null, 2));
+        }else{
           const response = {
             statusCode,
             headers,
             body: JSON.stringify(data.Items)
-        }
+        }}
         callback(null, response);
     }).promise();
+}
