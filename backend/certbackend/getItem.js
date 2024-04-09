1e0d0fbfcefa18e39267ca2a904cc63001573e4c
dlg94490 <150751446+dlg94490@users.noreply.github.com>
2024-04-09 18:46:04 -0500

    GetItem almost done
    It connects and it displays where the data should be but still have to make the data show up

--- backend/certbackend/getItem.js
+++ backend/certbackend/getItem.js
@@ -1,51 +1,78 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-//const { URLSearchParams } = require('url');
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-//const uuid = require('uuid'); //requre all fields***
-const requestTable = process.env.REQUEST_TABLE;
+    const AWS = require('aws-sdk');
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
+    
+    const requestTable = process.env.REQUEST_TABLE;
+    
+    exports.getItem = async (event, context, callback)  => {
+        let headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Credentials': true
+        };
+        let statusCode = 200;
+    
+        console.log("EVENT:::", JSON.stringify(event));
+    
+        const tableName = requestTable  // event.pathParameters.model
 
-module.exports.getItem = async (event, context, callback) => {
-    let headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true
-    };
-    let statusCode = 200;
+    const i8d = "i8d";
+   // table = requestTable;
+    // let table;
+    // switch (tableName) { //If you have other tables you would add them here as other case statements to reference that table.
+    //     case "error":
+    //         throw new Error("Unsupported resource:");
+    //     default:           
+    //         table = requestTable;
+    //         break;
+    // }
 
-   // const data = JSON.parse(Buffer.from(event.body, 'base64').toString());
-    console.log("EVENT:::", data);
+    // const params = {
+    //     TableName: tableName, 
+    //    //Key: {id:{'S' : i8d}}
+    //    FilterExpression: "id = :id",
+    //    ExpressionAttributeValues: {":id": {"S": i8d}} //{":id": "i8d"},
+    //     // ConsistentRead: true
+        
 
-    const tableName =/*requestTable; //*/ event.pathParameters.model;
-    const id = "id";
-    let table;
-    switch (tableName) { //If you have other tables you would add them here as other case statements to reference that table.
-        case "error":
-            throw new Error("Unsupported resource:");
-        default:           
-            table = requestTable;
-            break;
-    }
-
+    // }
     const params = {
-        TableName: table,
-        KeyConditionExpression: "id = :id",
-        ExpressionAttributeValues:{ ":id" : {S: id.toString() }} //{\":id\":{\"S\":\"i839d\"}
+        TableName: tableName,
+        FilterExpression: "id = :id",
+        ExpressionAttributeValues: {":id": {"S": i8d}}
     }
         
     
 
-    console.log("Getting individual Item from table:::", table);
+    console.log("Getting individual Item from table:::", tableName);
 
-    await dynamoDb.query(params, (err, data)=> { 
-        if (err) {
-          console.log("Error",  JSON.stringify(err, null, 2));
-        }else{
-          const response = {
+
+    await dynamoDb.scan(params, (error, data) => {
+        if (error) {
+            console.log('Scan failed. Error JSON:', JSON.stringify(error, null, 2));
+            callback(error);
+            return;
+        }
+        const response = {
             statusCode,
             headers,
             body: JSON.stringify(data.Items)
-        }}
+        }
         callback(null, response);
     }).promise();
-}
+};
+
+
+    
+    // await dynamoDb.query(params, (err, data)=> { 
+    //     if (err) {
+    //       console.log("Error",  JSON.stringify(err, null, 2));
+    //     }else{
+    //       const response = {
+    //         statusCode,
+    //         headers,
+    //         body: JSON.stringify(data.Items)
+    //     }}
+    //     callback(null, response);
+//     // }).promise();
+// }
